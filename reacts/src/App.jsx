6eb6bf7ed336e9e7ed7a40d6af9ea61749d6f050
lillypakiyam <85731775+lillypakiyam@users.noreply.github.com>
2023-09-18 11:14:59 +0530

    hello

---
+++ reacts/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import './profile'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <h1> Hello World</h1>
+}
+
+export default App
+
+// git remote add origin https://github.com/lillypakiyam/reactP.git
+// git branch -M main
+// git push -u origin main
